126c665bec9e13801c6699fde47a1a11b117ea40
Joseph DeCarlo <josephdecarlo@bziti.com>
2015-07-14 16:34:46 -0400

    Initial Commit

---
+++ SuperDuperLibrary/SuperDuperLibrary.h
@@ -0,0 +1,19 @@
+//
+//  SuperDuperLibrary.h
+//  SuperDuperLibrary
+//
+//  Created by Joseph DeCarlo on 7/14/15.
+//  Copyright (c) 2015 Baked Ziti, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SuperDuperLibrary.
+FOUNDATION_EXPORT double SuperDuperLibraryVersionNumber;
+
+//! Project version string for SuperDuperLibrary.
+FOUNDATION_EXPORT const unsigned char SuperDuperLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuperDuperLibrary/PublicHeader.h>
+
+
---
+++ SuperDuperWorker.swift
@@ -0,0 +1,16 @@
+//
+//  SuperDuperWorker.swift
+//  SuperDuperLibrary
+//
+//  Created by Joseph DeCarlo on 7/14/15.
+//  Copyright (c) 2015 Baked Ziti, LLC. All rights reserved.
+//
+
+public struct SuperDuperWorker {
+   
+    public func doSomethingSuper() -> String {
+        
+        return "This is something quite magnificent!"
+    }
+    
+}
